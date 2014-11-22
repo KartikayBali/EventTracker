e32d09133546e14e08bb0a5acf3db67da0f03752
Ratnesh <user@Ratnesh.local>
2014-11-22 20:06:16 +0530

    Initial Commit

---
+++ EventTracking/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EventTracking
+//
+//  Created by Ratnesh on 22/11/14.
+//  Copyright (c) 2014 Keepworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EventTracking/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EventTracking
+//
+//  Created by Ratnesh on 22/11/14.
+//  Copyright (c) 2014 Keepworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
